1a7b73618574e43394a977287a9ab21a2e6a01eb
Aladdin-2 <aladdin19.11.21@gmail.com>
2025-11-11 13:45:20 +0400

    ING-LAB-025: add RabbitMQ configuration and publisher

---
+++ src/main/java/az/ingress/config/RabbitMQConstants.java
@@ -0,0 +1,9 @@
+package az.ingress.config;
+
+public class RabbitMQConstants {
+
+    public static final String ORDER_EXCHANGE = "order.exchange";
+
+    public static final String ORDER_EVENT = "order.event";
+    public static final String ORDER_EVENT_DLQ = "order.event.dlq";
+}
